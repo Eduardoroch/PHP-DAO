609727754b693a95ad55408b00ac821caee6de1c
Eduardo Rocha Viana <eduardorochvian@gmail.com>
2019-01-07 12:25:49 -0300

    incrementando a classe Usuario

--- index.php
+++ index.php
@@ -2,17 +2,43 @@
 
 require_once("config.php");
 
+
 /*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
+
 */
 
+
+/*
+
 $root = new Usuario();
-
 $root->loadById(4);
-
 echo $root;
+
+*/
+
+
+/*
+
+$lista = Usuario::getList();
+echo json_encode($lista);
+
+*/
+
+
+/*
+
+$search = Usuario::search("jo");
+echo json_encode($search);
+
+*/
+
+$usuario = new Usuario();
+
+$usuario->login("Teste","159753");
+
+echo $usuario;
 
  ?>
